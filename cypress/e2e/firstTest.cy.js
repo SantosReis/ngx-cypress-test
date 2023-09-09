2adba32d85ac8f4e37e7236f56c54bc57fe703dc
Marcus Reis <marcus.reis@askblue.pt>
2023-09-09 12:38:06 +0100

    Types of Locators

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,51 +1,39 @@
-describe("Our first suite", () => {
+/// <reference types="cypress" />
 
-  describe("Our suite section", () => {
+describe('Our first suite', () => {
+  it('first test', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
 
-    beforeEach('code for every test', () => {
-      // repetitive code
-    })
+    //by tag name
+    cy.get('input')
 
-    it("some test name", () => {
+    //by ID
+    cy.get('#inputEmail')
 
-    })
-  
-    it("some test name", () => {
-  
-    })
-  
-    it("some test name", () => {
-  
-    })
+    //by class name
+    cy.get('.input-full-width')
 
+    //by attribute name
+    cy.get('[placeholder]')
+
+    //by attribute name and value
+    cy.get('[placeholder="Email"]')
+
+    //by class value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+    //by tag name and attribute with value
+    cy.get('input[placeholder="Email"]')
+
+    //by two different attributes
+    cy.get('[placeholder="Email"][type="Email"]')
+
+    //by tagname, attribute with value, ID and classname
+    cy.get('input[placeholder="Email"]#inputEmail.input-full-width')
+
+    //The most recommended way by Cypress (build own selector)
+    cy.get('[data-cy="imputEmail1"]')
   })
-
-  it("first test", () => {
-
-  })
-
-  it("second test", () => {
-
-  })
-
-  it("third test", () => {
-
-  })
-
-})
-
-describe("Our second suite", () => {
-
-  it("first test", () => {
-
-  })
-
-  it("second test", () => {
-
-  })
-
-  it("third test", () => {
-
-  })
-
 })
