31aab4d5f0f01980be441f274ff0cfa740e34787
Marcus Reis <marcus.reis@askblue.pt>
2023-09-10 20:52:07 +0100

    Web Tables

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -209,7 +209,7 @@ describe('Our first suite', () => {
     cy.get('[type="checkbox"]').eq(1).check({ force: true })
   })
 
-  it.only('list and dropdowns', () => {
+  it('list and dropdowns', () => {
     cy.visit('/')
 
     //1
@@ -242,6 +242,54 @@ describe('Our first suite', () => {
         )
         if (index < 3) {
           cy.wrap(dropdown).click()
+        }
+      })
+    })
+  })
+
+  it.only('Web tables', () => {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //1
+    cy.get('tbody')
+      .contains('tr', 'Larry')
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('.nb-edit').click()
+        cy.wrap(tableRow).find('[placeholder="Age"]').type('25')
+        cy.wrap(tableRow).find('.nb-checkmark').click()
+        cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+      })
+
+    //2
+    cy.get('thead').find('.nb-plus').click()
+    cy.get('thead')
+      .find('tr')
+      .eq(2)
+      .then((tableRow) => {
+        cy.wrap(tableRow).find('[placeholder="First Name"]').type('Artem')
+        cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Bondar')
+        cy.wrap(tableRow).find('.nb-checkmark').click()
+      })
+    cy.get('tbody tr')
+      .first()
+      .find('td')
+      .then((tableColumns) => {
+        cy.wrap(tableColumns).eq(2).should('contain', 'Artem')
+        cy.wrap(tableColumns).eq(3).should('contain', 'Bondar')
+      })
+
+    //3
+    const age = [20, 30, 40]
+    cy.wrap(age).each((age) => {
+      cy.get('thead [placeholder="Age"]').clear().type(age)
+      cy.wait(500)
+      cy.get('tbody tr').each((tableRow) => {
+        if (age == 200) {
+          cy.wrap(tableRow).should('contain', 'No data found')
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age)
         }
       })
     })
