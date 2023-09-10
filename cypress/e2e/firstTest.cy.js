3b202c6aab62c3960eba7f18dd3edee8768a5ff6
Marcus Reis <marcus.reis@askblue.pt>
2023-09-10 21:05:53 +0100

    PopUps and ToolTips

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -247,7 +247,7 @@ describe('Our first suite', () => {
     })
   })
 
-  it.only('Web tables', () => {
+  it('Web tables', () => {
     cy.visit('/')
     cy.contains('Tables & Data').click()
     cy.contains('Smart Table').click()
@@ -293,5 +293,43 @@ describe('Our first suite', () => {
         }
       })
     })
+  })
+
+  it.only('popups and tooltips', function () {
+    cy.visit('/')
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Tooltip').click()
+
+    cy.contains('nb-card', 'Colored Tooltips').contains('Default').click()
+    cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+  })
+
+  it('dialog box', function () {
+    cy.visit('/')
+    cy.contains('Tables & Data').click()
+    cy.contains('Smart Table').click()
+
+    //1
+    // cy.get('tbody tr').first().find('.nb-trash').click()
+    // cy.on('window:confirm', (confirm) => {
+    //   expect(confirm).to.equal('Are you sure you want to delete?')
+    // })
+
+    // 2
+    // const stub = cy.stub()
+    // cy.on('window:confirm', stub)
+    // cy.get('tbody tr')
+    //   .first()
+    //   .find('.nb-trash')
+    //   .click()
+    //   .then(() => {
+    //     expect(stub.getCall(0)).to.be.calledWith(
+    //       'Are you sure you want to delete?'
+    //     )
+    //   })
+
+    // 3
+    cy.get('tbody tr').first().find('.nb-trash').click()
+    cy.on('window:confirm', (confirm) => false)
   })
 })
